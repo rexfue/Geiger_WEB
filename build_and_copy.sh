5138d6d14c8f33721f718730589919690649ee5e
Reinhard X. Fürst <rexfue@gmail.com>
2018-09-14 14:44:52 +0200

    public/help.html and helpmap.html    - link to impressum added
    build_and_copy
       - made for new Dockerfile
    
    Dockerfie_fst-web and Dockerfile-fstst-web
       - made to be usable for new build_and_copy
    
    .gitignore
       - added an few files

--- build_and_copy.sh
+++ build_and_copy.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-# Build Docker-Container for fst-web ans fstst-web
+# Build Docker-Container
 #
-# Call: buildit.sh [TST|WEB]
+# Call: buildit.sh name [target]
 #
-set -x
+# The Dockerfile_fst-data mus be named like Dockerfile_name
+#
+# 2018-09-14  rxf
+#
+#set -x
 if [ $# -lt 1 ]
   then
-    echo "Usage buildit.sh <TST|WEB> [Target]"
-    echo "Argument MUST be TST or WEB"
-    echo " Second Argument: Where to copy the container to (optional)"
+    echo "Usage buildit_and_copy.sh name [target]"
+    echo "   name:   Name of container"
+    echo "   target: Where to copy the container to (optional)"
     exit
 fi
 
-fn="fst"
-
-if [ "$1" == "TST" ]
-   then
-        fn="fstst"
-fi
-
-docker build -f Dockerfile_$1 -t ${fn}-web .
+docker build -f Dockerfile_$1 -t $1 .
 
 if [ $2 != "" ]
 then
-    docker save ${fn}-web | bzip2 | pv | ssh $2 'bunzip2 | docker load'
+    docker save $1 | bzip2 | pv | ssh $2 'bunzip2 | docker load'
 fi
